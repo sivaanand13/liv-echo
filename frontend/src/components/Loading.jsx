b95ffdd5b76f38bfc7a459dd4c7767f7b96ca227
sivaanand13 <SIVAANAND.IN@GMAIL.COM>
2025-05-23 07:44:55 -0400

    removing ;

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -2,14 +2,14 @@ import { CircularProgress } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 
 const StyledCircularProgress = styled(CircularProgress)(({ theme }) => ({
-  overflowY: "scroll";
+  overflowY: "scroll",
   margin: "auto",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   minHeight: "100vh",
   width: "20%",
-  height: "20%"
+  height: "20%",
 }));
 
 function Loading() {
